b5bd7a75e276e63114ea2928cdb382d1fb683b1c
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-12 20:32:56 +0000

    Fix: error when stack empty pchar

--- opcodes_2.c
+++ opcodes_2.c
@@ -22,7 +22,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		free_all(stack, EXIT_FAILURE);
 	}
 
